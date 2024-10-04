7af5334373a12a24a8170b77143b8abf1e028d29
MadSons <madsenc004@gmail.com>
2024-10-04 14:43:06 -0400

    moved code to source. Build and tests work

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/src/**/*.test.ts'],
+  testMatch: ['/**/*.test.ts'],
   collectCoverage: true,
   coverageReporters: ['text', 'lcov'],
   coverageThreshold: {
